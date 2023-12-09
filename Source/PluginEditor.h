1ebc0dcdabdcd7971bc4ce1269c761b6d71274dc
Valto Moisio <valto.moisio@pilvit.fi>
2023-12-09 21:26:13 +0200

    Add rotary sliders

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,16 +11,85 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct LookAndFeel : juce::LookAndFeel_V4 {
+    void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos,
+        		float rotaryStartAngle, float rotaryEndAngle, juce::Slider&) override;
+
+    void drawToggleButton(juce::Graphics& g, juce::ToggleButton& toggleButton, bool shouldDrawButtonAsHighlighted,
+        				bool shouldDrawButtonAsDown) override;
+};
+
+struct RotarySliderWithLabels : juce::Slider {
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+        juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+            juce::Slider::TextEntryBoxPosition::NoTextBox),
+        param(&rap),
+        suffix(unitSuffix)
+    {
+		setLookAndFeel(&lnf);
+	}
+
+    ~RotarySliderWithLabels() {
+		setLookAndFeel(nullptr);
+	}
+
+    struct LabelPos {
+		float pos;
+		juce::String label;
+	};
+
+	juce::Array<LabelPos> labels;
+
+	void paint(juce::Graphics& g) override;
+
+	juce::Rectangle<int> getSliderBounds() const;
+
+	int getTextHeight() const { return 14; }
+	juce::String getDisplayString() const;
+
+private:
+    LookAndFeel lnf;
+
+	juce::RangedAudioParameter* param;
+	juce::String suffix;
+};
+
+struct PowerButton : juce::ToggleButton {};
+
 //==============================================================================
 
-struct Placeholder : juce::Component {
-    Placeholder();
+struct RotarySlider : juce::Slider {
+    RotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+        		juce::Slider::TextEntryBoxPosition::NoTextBox) {}
+};
 
-    void paint(juce::Graphics& g) override {
-        g.fillAll(customColor);
-    }
+template<typename Attachment, typename APVTS, typename Params, typename ParamName, typename SliderType>
+void makeAttachment(std::unique_ptr<Attachment>& attachment,
+    APVTS& apvts,
+    const Params& params,
+    const ParamName& name,
+    SliderType& slider)
+{
+    attachment = std::make_unique<Attachment>(apvts, params.at(name), slider);
+}
 
-    juce::Colour customColor;
+struct Logo : juce::Component {
+    void paint(juce::Graphics& g) override;
+};
+
+struct ControlKnobs : juce::Component {
+    ControlKnobs(juce::AudioProcessorValueTreeState& apvts);
+    void paint(juce::Graphics& g) override;
+    void resized() override;
+private:
+    RotarySlider attackSlider, releaseSlider, thresholdSlider, ratioSlider;
+
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    std::unique_ptr<Attachment> attackAttachment, releaseAttachment, thresholdAttachment, ratioAttachment;
+};
+
+struct GainControls : juce::Component {
+	void paint(juce::Graphics& g) override;
 };
 
 class KompuraAudioProcessorEditor  : public juce::AudioProcessorEditor
@@ -38,7 +107,9 @@ private:
     // access the processor object that created it.
     KompuraAudioProcessor& audioProcessor;
 
-    Placeholder logo, inputGainControl, outputGainControl, attackReleaseControls, thresholdRatioControls;
+    GainControls inputGainControl, outputGainControl;
+    ControlKnobs controlKnobs { audioProcessor.apvts };
+    Logo logo;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (KompuraAudioProcessorEditor)
 };
